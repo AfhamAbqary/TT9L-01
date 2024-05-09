c13faf4092f3847e01cbbe15bb61d73d2eea8074
mana <144979437+AfhamAbqary@users.noreply.github.com>
2024-05-09 22:19:13 +0800

    Created base for project
    Added base for project and the files for python and html

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("home"))
+
+if __name__ == "__main__":
+    app.run(debug=True)
