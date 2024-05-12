1c8f41b0c83263eede00fec4266b441987bad78f
mana <144979437+AfhamAbqary@users.noreply.github.com>
2024-05-12 11:23:46 +0800

    Create user page and logout feature

--- user/user.py
+++ user/user.py
@@ -7,7 +7,18 @@ client = PocketBase('https://alma-mater.pockethost.io')
 
 @user.route("/")
 def home():
-    return "<h1>UNDER CONSTRUCTION</h1>"
+    if "login" in session:
+        if client.auth_store.base_model == None:
+            client.collection("users").auth_with_password(session["login"][0], session["login"][1])
+        userdata = client.auth_store.base_model #Get user data from session saved
+        username = userdata.username
+        email = userdata.email
+        teacher = "Teacher" if userdata.teacher else "Student"
+        classes= [client.collection("Class").get_one(f"{i}").title for i in userdata.classes] #Get every classes registered for user
+        return render_template("user.html", username=username, email=email, Teacher=teacher, Class=classes)
+    else:
+        return render_template("user.html", username="NOT LOGGED IN OR NO INTERNET CONNECTION")
+    
 
 @user.route("/login", methods=['POST', 'GET'])
 def login():
@@ -15,10 +26,12 @@ def login():
         user = request.form['username']
         password = request.form['password']
         try:
-            client.collection("users").auth_with_password(user, password)
-            return redirect(url_for(user))
-        except:
-            print("Information wrong")
+            client.collection("users").auth_with_password(user, password) #Authorize login for data fetching and writing to database
+            session["login"] = [user,password] #Save login info to session
+            session["lasturl"] = "user.home"
+            return redirect(url_for("user.home"))
+        except Exception as e:
+            print("Error:", e) #Prints error for message flashing later :TO DO
             return render_template("login.html")
     else:
         return render_template("login.html")
@@ -45,9 +58,17 @@ def signup():
                 }
             )
             print("Success")
+            session["lasturl"] = "user.home"
             return redirect(url_for("user.home"))
-        except:
-            print("Something went wrong!")
+        except Exception as e:
+            for i in e.__dict__["data"]["data"]:
+                print(f'{i}:' + e.__dict__["data"]["data"][i]["message"])
             return render_template("signup.html")
     else:
         return render_template("signup.html")
+    
+@user.route("/logout", methods=["POST","GET"])
+def logout():
+    client.auth_store.clear()
+    session.pop("login", None)
+    return redirect(url_for(session["lasturl"]))
