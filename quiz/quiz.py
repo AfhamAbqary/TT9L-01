61ee85eaaea0af6d089ee93ad14b8e04c324bffb
AishQayyim <167382407+AishQayyim@users.noreply.github.com>
2024-05-15 14:11:31 +0800

    ADDED QUIZ PAGE
    base

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -1,16 +1,15 @@
 from flask import Blueprint, render_template, request, session, redirect, url_for
-from pocketbase import PocketBase #MAKE SURE TO INSTALL Pocketbase BEFOREHAND
+from pocketbase import PocketBase 
 from pocketbase.client import FileUpload
 from ..extensions import client
 
-classroom_bp = Blueprint("classroom", __name__, static_folder="", template_folder="website")
+quiz_bp = Blueprint("quiz", __name__, static_folder="", template_folder="website")
 
-@classroom_bp.route("/")
+@quiz_bp.route("/")
 def home():
     if client.auth_store.base_model != None:
         userdata = client.auth_store.base_model
         classes= [client.collection("Class").get_one(f"{i}").title for i in userdata.classes]
-        return render_template("classroom.html", Class=classes)
+        return render_template("quiz.html", quiz=classes)
     else:
         return "<h1>UNDER CONSTRUCTION</h1>"
-    
