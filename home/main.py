6e8e386baeb0d21dbc964eb1a10f4594d489e4f4
mana <144979437+AfhamAbqary@users.noreply.github.com>
2024-06-22 16:15:55 +0800

    Update main.py

--- home/main.py
+++ home/main.py
@@ -1,11 +1,26 @@
 from flask import Flask, Blueprint, session, render_template, redirect, url_for
 from ..extensions import client
 
-home_bp = Blueprint("home", __name__, static_folder="", template_folder="templates")
+home_bp = Blueprint("home", __name__)
 
 @home_bp.route("/") #Home page
 def home():
     try:
-        return render_template("index.html")
+        if client.auth_store.base_model != None:
+            login = True
+            userdata = client.auth_store.base_model
+            if userdata.teacher:
+                classes= client.collection("Class").get_full_list(query_params={
+                    'filter': f'Teachers.id~"{userdata.id}"'
+                })
+            else:
+                classes= client.collection("Class").get_full_list(query_params={
+                    'filter': f'Students.id?~"{userdata.id}"'
+                })
+
+            return render_template("index.html", classroom=classes, name=userdata.username, login=login)
+        else:
+            login = False
+            return render_template("index.html", login=login)
     except Exception as e:
         return render_template("Error.html", error=e)
