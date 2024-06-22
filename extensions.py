b04c17f61633f01e4d50067fc13a50db4d1fc1b4
mana <144979437+AfhamAbqary@users.noreply.github.com>
2024-06-22 16:11:14 +0800

    Update extensions.py

--- extensions.py
+++ extensions.py
@@ -1,8 +1,10 @@
 from pocketbase import PocketBase
+from pocketbase.client import FileUpload
 from flask_simple_crypt import SimpleCrypt
 import os
 from dotenv import load_dotenv
 from flask_ckeditor import CKEditor
+import random
 
 #Forms class
 ckeditor = CKEditor()
